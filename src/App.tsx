6c3c0acfc6effc47392fb43673f0fefe25593395
bipingurung01 <bipin@ramailo.tech>
2024-04-19 23:11:02 +0545

    store config with authstate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth/auth"
+import { login, logout } from "./features/authSlice"
+import { Footer, Header } from "./components"
 const App = () => {
-  return (
-    <div className="underline text-2xl">App</div>
-  )
+  const [loading, setLoading] = useState<Boolean>(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+  return !loading ? (<div className="min-h-screen flex flex-wrap content-between bg-gray-300">
+    <div className="w-full block">
+      <Header />
+        <main>
+          {/* Todo: Configure Outlet From react-router-dom */}
+          TODO
+        </main>
+      <Footer />
+    </div>
+  </div>) : null
 }
 
 export default App
